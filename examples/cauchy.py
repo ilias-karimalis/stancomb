c9d56e858e4580adff0aaf587dc3d1c2a4c1e996
Ilias Karimalis <39017822+ilias-karimalis@users.noreply.github.com>
2022-07-11 16:15:32 -0700

    Update cauchy.py

--- examples/cauchy.py
+++ examples/cauchy.py
@@ -1,12 +1,12 @@
 from stancomb import mixture_model
 
 print(mixture_model(
-	"real gap;",	 # Data
-	"", # Transformed Data
-	"real alpha;", # Shared Parameters
-	"", 	# Transformed Shared Parameters
-	[""],	# Non Shared Parameters
-	[""], 	# Non Shared Transformed Parameters
+	"real gap;",	 					 # Data
+	"", 							 # Transformed Data
+	"real alpha;", 						 # Shared Parameters
+	"", 							 # Transformed Shared Parameters
+	[""],							 # Non Shared Parameters
+	[""], 							 # Non Shared Transformed Parameters
 	["alpha ~ cauchy(-gap, 0.2)\nalpha ~ cauchy(gap, 0.2);", # Model 1
-	 "alpha ~ normal(0, 5);"]	# Model 2
+	 "alpha ~ normal(0, 5);"]				 # Model 2
 ))
